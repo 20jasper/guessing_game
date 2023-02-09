98461c2225b6409eb7cf595241f36960d778e8c0
Jacob Asper <jacobasper191@gmail.com>
2023-02-09 00:21:35 -0500

    get guess from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+	println!("Guess a number 1-100");
+
+	let mut guess = String::new();
+
+	io::stdin()
+		.read_line(&mut guess)
+		.expect("uh oh");
+
+	println!("your guess is {guess}")
 }
