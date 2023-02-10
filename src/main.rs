c2b952beb6d9c6e07edc8e9e65bed94023461354
Jacob Asper <jacobasper191@gmail.com>
2023-02-09 19:14:10 -0500

    Loop until right num is guessed

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use rand::Rng;
-use std::io;
+use std::{cmp::Ordering, io};
 
 fn main() {
 	let secret_number = rand::thread_rng().gen_range(1..=100);
 
 	println!("secret number is {secret_number}");
 
-	println!("Guess a number 1-100");
+	loop {
+		println!("Guess a number 1-100");
 
-	let mut guess = String::new();
+		let mut guess = String::new();
 
-	io::stdin()
-		.read_line(&mut guess)
-		.expect("uh oh");
+		io::stdin()
+			.read_line(&mut guess)
+			.expect("uh oh");
 
-	println!("your guess is {guess}");
+		let guess: u32 = guess
+			.trim()
+			.parse()
+			.expect("Please type a number!");
+
+		println!("your guess is {guess}");
+
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("Too small!"),
+			Ordering::Greater => println!("Too big!"),
+			Ordering::Equal => {
+				println!("You win!");
+				break;
+			}
+		};
+	}
 }
